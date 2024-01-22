237e07c6c77505f6a6cbf90d1b5b50390efee2f9
udigcowan <will.cowan@udig.com>
2024-01-22 13:42:21 -0600

    Input Component WIP

--- react-appwrite-app/src/components/Input.jsx
+++ react-appwrite-app/src/components/Input.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-function Input() {
-  return (
-    <div>Input</div>
-  )
-}
+const Input = React.forwardRef( function Input({label, type = 'text', className='', ...props}, refs){})
 
 export default Input
